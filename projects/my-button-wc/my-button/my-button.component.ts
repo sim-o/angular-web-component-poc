ab465093b2c9d8449476350a639309c664a6db55
Simon Kerle <skerle@vmware.com>
2024-04-19 09:50:58 +1000

    add fetch

--- projects/my-button-wc/my-button/my-button.component.ts
+++ projects/my-button-wc/my-button/my-button.component.ts
@@ -1,14 +1,26 @@
-import { ViewEncapsulation } from '@angular/compiler';
-import { Component } from '@angular/core';
+import {Component, HostBinding, ViewEncapsulation} from '@angular/core';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-my-button',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './my-button.component.html',
   styleUrl: './my-button.component.css',
   encapsulation: ViewEncapsulation.ShadowDom
 })
 export class MyButtonComponent {
+  json?: string;
 
+  @HostBinding('style')
+  style = 'border: 1px solid red; display; inline-block; padding: 12px; margin: 12px;';
+
+  doFetch() {
+    fetch('http://localhost:8000/remote.json')
+      .catch((e: Error) => this.json = e.message)
+      .then((r) => r instanceof Response ? r.text() : r)
+      .then(t => this.json = t);
+  }
 }
